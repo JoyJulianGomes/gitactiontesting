a2d421739d7acf49dc93238a27d68351aacf52a5
Joy Julian Gomes <joyjuliangomes@gmail.com>
2020-11-05 21:12:22 +0900

    phase 8

--- .github/workflows/homepageaddress.js
+++ .github/workflows/homepageaddress.js
@@ -1,10 +1,13 @@
+let subdomain_name = process.argv[2];
+let folder_name = process.argv[3];
 let pj = require("../../package.json");
 let fs = require("fs");
 console.log("before adding homepage:", pj.homepage);
-pj.homepage = "x/stage/bug";
+pj.homepage = ".";
 let json = JSON.stringify(pj, null, 2);
 fs.writeFile("../../package.json", json, "utf-8", () => {
-    console.log("added homepage to package.json");
+	console.log("added homepage to package.json");
 });
-pj = require('../../package.json');
+pj = require("../../package.json");
 console.log("after adding homepage:", pj.homepage);
+console.log("package.json", pj);
