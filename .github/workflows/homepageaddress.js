4df7e6299126ac05e00c89f8c11c3786d7449d51
Joy Julian Gomes <joyjuliangomes@gmail.com>
2020-11-05 20:15:05 +0900

    phase 4

---
+++ .github/workflows/homepageaddress.js
@@ -0,0 +1,10 @@
+let pj = require("s../../package.json");
+let fs = require("fs");
+console.log("before adding homepage:", pj.homepage);
+pj.homepage = "x/stage/bug";
+let json = JSON.stringify(pj, null, 4);
+fs.writeFile("./package.json", json, "utf-8", () => {
+    console.log("added homepage to package.json");
+});
+pj = require('./package.json');
+console.log("after adding homepage:", pj.homepage);
