a8f02a7d76b99b4a1a7cc14945bc023f7d38ca17
Joy Julian Gomes <joyjuliangomes@gmail.com>
2020-11-06 12:11:04 +0900

    phase 9

--- .github/workflows/homepageaddress.js
+++ .github/workflows/homepageaddress.js
@@ -3,7 +3,7 @@ let folder_name = process.argv[3];
 let pj = require("../../package.json");
 let fs = require("fs");
 console.log("before adding homepage:", pj.homepage);
-pj.homepage = ".";
+pj.homepage = subdomain_name+"/"+folder_name;
 let json = JSON.stringify(pj, null, 2);
 fs.writeFile("../../package.json", json, "utf-8", () => {
 	console.log("added homepage to package.json");
