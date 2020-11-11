414654eacd51a2e759aab8e736e098e3232f15b3
Joy Julian Gomes <joyjuliangomes@gmail.com>
2020-11-11 17:39:33 +0900

    phase 13

--- .github/workflows/homepageaddress.js
+++ .github/workflows/homepageaddress.js
@@ -1,13 +1,20 @@
-let subdomain_name = process.argv[2];
-let folder_name = process.argv[3];
-let pj = require("../../package.json");
-let fs = require("fs");
-console.log("before adding homepage:", pj.homepage);
-pj.homepage = subdomain_name+"/"+folder_name;
-let json = JSON.stringify(pj, null, 2);
-fs.writeFile("../../package.json", json, "utf-8", () => {
+function beforeWrite() {
+	let pj = require("../../package.json");
+	let subdomain_name = process.argv[2];
+	let folder_name = process.argv[3];
+	let fs = require("fs");
+	console.log("subdomain: ", subdomain_name);
+	console.log("before adding homepage: ", pj.homepage);
+	pj.homepage = subdomain_name + "/" + folder_name;
+	console.log("homepage value: ", pj.homepage);
+	let json = JSON.stringify(pj, null, 2);
+	fs.writeFileSync("../../package.json", json)
+}
+function afterWrite() {
 	console.log("added homepage to package.json");
-});
-pj = require("../../package.json");
-console.log("after adding homepage:", pj.homepage);
-console.log("package.json", pj);
+	pj_after = require("../../package.json");
+	console.log("after adding homepage:", pj_after.homepage);
+	console.log("package.json", pj_after);
+}
+beforeWrite();
+afterWrite();
